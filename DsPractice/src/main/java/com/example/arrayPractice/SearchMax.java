7c6c9c59adb159f3ff1a4bde71cb9838621846a7
siddharth neekhara <siddharthneekhara@siddharths-MacBook-Pro.local>
2022-08-07 16:09:05 +0530

    array changes

--- DsPractice/src/main/java/com/example/arrayPractice/SearchMax.java
+++ DsPractice/src/main/java/com/example/arrayPractice/SearchMax.java
@@ -2,16 +2,14 @@ package com.example.arrayPractice;
 
 public class SearchMax {
 
-	public static void main(String[] args) {
-		int a[] = { 1, 7, 2, 4, 5, 9, 10 };
-		int max=a[0];
-		for(int i=0;i<a.length;i++) {
-			if(max<a[i]) {
-				max=a[i];
-			}
-		}
-		System.out.println("Max no is:  "+max);
-				
-	}
-
+    public static void main(String[] args) {
+        int a[] = {1, 7, 2, 4, 5, 9, 10};
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        System.out.println("Max no is:  " + max);
+    }
 }
--- DsPractice/src/main/java/com/example/arrayPractice/SubarraySum.java
+++ DsPractice/src/main/java/com/example/arrayPractice/SubarraySum.java
@@ -1,7 +1,9 @@
 package com.example.arrayPractice;
 
-class SubarraySum {
-    int subArraySum(int arr[], int n, int sum) {
+public class SubarraySum {
+
+    public static int subArraySum(int arr[], int n, int sum) {
+
         int curr_sum = arr[0], start = 0, i;
         for (i = 1; i <= n; i++) {
             while (curr_sum > sum && start < i - 1) {
@@ -10,22 +12,21 @@ class SubarraySum {
             }
             if (curr_sum == sum) {
                 int p = i - 1;
-                System.out.println("Sum found between indexes " + start
-                        + " and " + p);
+                System.out.println("Sum found between indexes " + start + " and " + p);
                 return 1;
             }
-            if (i < n)
+            if (i < n) {
                 curr_sum = curr_sum + arr[i];
+            }
         }
         System.out.println("No subarray found");
         return 0;
     }
 
     public static void main(String[] args) {
-        SubarraySum arraysum = new SubarraySum();
         int arr[] = {15, 2, 4, 8, 9, 5, 10, 23};
         int n = arr.length;
         int sum = 23;
-        arraysum.subArraySum(arr, n, sum);
+        subArraySum(arr, n, sum);
     }
 } 

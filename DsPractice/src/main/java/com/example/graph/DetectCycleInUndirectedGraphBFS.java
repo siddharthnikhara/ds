484564d8967ed7a4bfffb3d6cc9be59cdb9cc362
siddharth neekhara <siddharthnikhara>
2022-08-25 20:20:10 +0530

    Graph

--- DsPractice/src/main/java/com/example/graph/DetectCycleInUndirectedGraphBFS.java
+++ DsPractice/src/main/java/com/example/graph/DetectCycleInUndirectedGraphBFS.java
@@ -4,65 +4,58 @@ import java.util.*;
 
 public class DetectCycleInUndirectedGraphBFS {
 
-    static Map<Integer, ArrayList<Integer>> map = new HashMap();
-
-    static void addEdge(int u, int v, boolean flag) {
+    static void addEdge(int u, int v, boolean flag, Map<Integer, ArrayList<Integer>> map) {
         map.get(u).add(v);
         if (flag == false) {
             map.get(v).add(u);
         }
     }
 
-    static boolean isCyclicConntected(int s, int V, boolean visited[]) {
-        int parent[] = new int[V];
-        Arrays.fill(parent, -1);
+    static boolean isCyclicConntected(int key, Map<Integer, ArrayList<Integer>> map, Map<Integer, Boolean> visited) {
+        Map<Integer, Integer> parent = new HashMap<>();
         Queue<Integer> q = new LinkedList<>();
-        visited[s] = true;
-        q.add(s);
+        parent.put(key, -1);
+        visited.put(key, true);
+        q.add(key);
         while (!q.isEmpty()) {
-            int u = q.remove();
-            for (int i = 0; i < map.get(u).size(); i++) {
-                int v = map.get(u).get(i);
-                if (!visited[v]) {
-                    visited[v] = true;
-                    q.add(v);
-                    parent[v] = u;
-                } else if (parent[u] != v) return true;
-            }
-        }
-        return false;
-    }
-
-
-    static boolean isCyclicDisconntected(int V) {
-        boolean visited[] = new boolean[V];
-        Arrays.fill(visited, false);
-        for (int i = 0; i < V; i++) {
-            if (!visited[i] && isCyclicConntected(i, V, visited)) {
-                return true;
+            int front = q.remove();
+            for (int neighour : map.get(front)) {
+                if (visited.getOrDefault(neighour, false) && neighour != parent.get(front)) {
+                    return true;
+                } else if (!visited.getOrDefault(neighour, false)) {
+                    q.add(neighour);
+                    visited.put(neighour, true);
+                    parent.put(neighour, front);
+                }
             }
         }
         return false;
     }
 
     public static void main(String arg[]) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap();
+        Map<Integer, Boolean> visited = new HashMap();
         for (int i = 0; i < 5; i++) {
             map.put(i, new ArrayList<Integer>());
         }
-        addEdge(1, 0, false);
-        addEdge(0, 2, false);
-        addEdge(2, 1, false);
-        addEdge(0, 3, false);
-        addEdge(3, 4, false);
-
+        addEdge(1, 0, false, map);
+        addEdge(0, 2, false, map);
+        addEdge(2, 1, false, map);
+        addEdge(0, 3, false, map);
+        addEdge(3, 4, false, map);
+        boolean ans = false;
 //        addEdge(0, 1, false);
 //        addEdge(1, 2, false);
 //        addEdge(2, 0, false);
 //        addEdge(2, 3, false);
-        if (isCyclicDisconntected(5)) {
+        for (int i = 0; i < 5; i++) {
+            ans = isCyclicConntected(i, map, visited);
+        }
+        if (ans) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
     }
 }
+
--- DsPractice/src/main/java/com/example/graph/DetectCycleInUndirectedGraphDFS.java
+++ DsPractice/src/main/java/com/example/graph/DetectCycleInUndirectedGraphDFS.java
@@ -4,41 +4,23 @@ import java.util.*;
 
 public class DetectCycleInUndirectedGraphDFS {
 
-    static Map<Integer, Boolean> visited = new HashMap<>();
-    static Map<Integer, ArrayList<Integer>> map = new HashMap();
-    static Map<Integer, ArrayList<Integer>> map1 = new HashMap();
-
-
-    static void addEdge(int u, int v, boolean flag) {
+    static void addEdge(int u, int v, boolean flag, Map<Integer, ArrayList<Integer>> map) {
         map.get(u).add(v);
         if (flag == false) {
             map.get(v).add(u);
         }
     }
 
-    static Boolean isCyclicUtil(int v, int parent) {
-        visited.put(v, true);
-        Integer i;
-        Iterator<Integer> it = map.get(v).iterator();
-        while (it.hasNext()) {
-            i = it.next();
-            if (!visited.getOrDefault(i, false)) {
-                if (isCyclicUtil(i, v)) {
+    static Boolean isCyclicConntected(int key, int parent, Map<Integer, Boolean> visited, Map<Integer, ArrayList<Integer>> map) {
+        visited.put(key, true);
+        for (int neighour : map.get(key)) {
+            if (!visited.getOrDefault(neighour, false)) {
+                boolean isdetected = isCyclicConntected(neighour, key, visited, map);
+                if (isdetected) {
                     return true;
                 }
-            } else if (i != parent) {
+            } else if (neighour != parent) {
                 return true;
-            }
-        }
-        return false;
-    }
-
-    static Boolean isCyclic() {
-        for (int u = 0; u < map.size(); u++) {
-            if (!visited.getOrDefault(u, false)) {
-                if (isCyclicUtil(u, -1)) {
-                    return true;
-                }
             }
         }
         return false;
@@ -46,32 +28,24 @@ public class DetectCycleInUndirectedGraphDFS {
 
     public static void main(String[] args) {
         int V = 5;
+        Map<Integer, Boolean> visited = new HashMap<>();
+        Map<Integer, ArrayList<Integer>> map = new HashMap();
         for (int i = 0; i < V; i++) {
             map.put(i, new ArrayList<Integer>());
         }
-        DetectCycleInUndirectedGraphDFS g1 = new DetectCycleInUndirectedGraphDFS();
-        addEdge(1, 0, false);
-        addEdge(0, 2, false);
-        addEdge(2, 1, false);
-        addEdge(0, 3, false);
-        addEdge(3, 4, false);
-        if (g1.isCyclic()) {
-            System.out.println("Graph  contains cycle");
-        } else {
-            System.out.println("Graph doesn 't contains cycle");
+        addEdge(1, 0, false, map);
+        addEdge(0, 2, false, map);
+        addEdge(2, 4, false, map);
+        addEdge(0, 3, false, map);
+        addEdge(3, 4, false, map);
+        boolean ans = false;
+        for (int i = 0; i < V; i++) {
+            ans = isCyclicConntected(0, -1, visited, map);
         }
-
-        int K = 3;
-        for (int i = 0; i < K; i++) {
-            map1.put(i, new ArrayList<Integer>());
-        }
-        DetectCycleInUndirectedGraphDFS g2 = new DetectCycleInUndirectedGraphDFS();
-        addEdge(0, 1, false);
-        addEdge(1, 2, false);
-        if (g2.isCyclic()) {
-            System.out.println("Graph contains cycle");
+        if (ans) {
+            System.out.println("Yes");
         } else {
-            System.out.println("Graph doesn 't contains cycle");
+            System.out.println("No");
         }
     }
 }
